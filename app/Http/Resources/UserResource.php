6ba36adc70edc4eedcc18a0f954d2897dd1b70c6
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-10-19 12:08:09 +0800

    Fix issue on user address data response

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,24 +14,29 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
+        $data = [
             'id'         => $this->id,
             'username'   => $this->username,
             'first_name' => $this->first_name,
             'last_name'  => $this->last_name,
             'dob'        => $this->dob,
             'email'      => $this->email,
-            'address'    => [
-                'address_id'    => $this->address->id,
-                'address_1'     => $this->address->address_1,
-                'address_2'     => $this->address->address_2,
-                'city'          => $this->address->city,
-                'state'         => $this->address->state,
-                'country'       => $this->address->country,
-                'zip_code'      => $this->address->zip_code,
-            ],
             'created_at' => $this->created_at->toW3cString(),
             'updated_at' => $this->updated_at->toW3cString(),
         ];
+
+        if ($this->address) {
+            $data['address']    = [
+                'address_id' => $this->address->id,
+                'address_1'  => $this->address->address_1,
+                'address_2'  => $this->address->address_2,
+                'city'       => $this->address->city,
+                'state'      => $this->address->state,
+                'country'    => $this->address->country,
+                'zip_code'   => $this->address->zip_code,
+            ];
+        }
+
+        return $data;
     }
 }
