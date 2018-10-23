819c2f91a2484430911bb7beb422ee12414d139c
paulotra <trajanopaulo10@gmail.com>
2018-10-23 09:25:04 +0800

    [PROP-25] Fix Carousel on Build (#23)

--- frontend/src/components/Carousel.vue
+++ frontend/src/components/Carousel.vue
@@ -1,10 +1,10 @@
 <template>
-  <carousel :navigationEnabled="true" :perPage="4" :perPageCustom="[[0, 1], [768, 2], [992, 3], [1200, 4]]">
-    <slide><card/></slide>
-    <slide><card/></slide>
-    <slide><card/></slide>
-    <slide><card/></slide>
-    <slide><card/></slide>
+  <carousel class="carousel" :paginationEnabled="false" :navigationEnabled="true" :perPage="4" :perPageCustom="[[0, 1], [768, 2], [992, 3], [1200, 4]]">
+    <slide class="carousel-inner"><card/></slide>
+    <slide class="carousel-inner"><card/></slide>
+    <slide class="carousel-inner"><card/></slide>
+    <slide class="carousel-inner"><card/></slide>
+    <slide class="carousel-inner"><card/></slide>
   </carousel>
 </template>
 
@@ -22,42 +22,38 @@ export default {
 </script>
 
 <style lang="scss">
-.VueCarousel-inner {
-  .VueCarousel-slide {
+.carousel {
+  .carousel-inner {
     padding: 12.5px;
     backface-visibility: unset;
   }
-}
 
-.VueCarousel-pagination {
-  display: none;
-}
-
-.VueCarousel-navigation {
-  button.VueCarousel-navigation-button {
-    @apply text-transparent rounded-full border-solid border-2 border-green;
-    font-family: 'icomoon';
-    height: 42px;
-    width: 42px;
-    padding: 0;
-
-    &::before {
-      @apply text-green;
-    }
-
-    &.VueCarousel-navigation-prev {
-      left: -15px;
+  :last-child {
+    button {
+      @apply text-transparent rounded-full border-solid border-2 border-green;
+      font-family: 'icomoon';
+      height: 42px;
+      width: 42px;
+      padding: 0;
 
       &::before {
-        content: '\e905';
+        @apply text-green;
       }
-    }
 
-    &.VueCarousel-navigation-next {
-      right: -15px;
+      &:first-child {
+        left: -15px;
 
-      &::before {
-        content: '\e906';
+        &::before {
+          content: '\e905';
+        }
+      }
+
+      &:last-child {
+        right: -15px;
+
+        &::before {
+          content: '\e906';
+        }
       }
     }
   }
