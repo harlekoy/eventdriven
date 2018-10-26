1baf3c16724f6f1be2bcca516be963ea27e589dd
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-10-26 18:18:25 +0800

    Added horizon and totem (#41)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Horizon\Horizon;
+use Studio\Totem\Totem;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Totem::auth(function($request) {
+            return Auth::guard('admins')->check();
+        });
+
+        Horizon::auth(function ($request) {
+            return Auth::guard('admins')->check();
+        });
     }
 
     /**
