3faf9061e7c31e55009001db589bd453f2285119
paulotra <trajanopaulo10@gmail.com>
2018-10-29 16:46:14 +0800

    Add Global Date Picker (#42)

---
+++ frontend/src/components/BaseDatePicker.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-date-picker v-model="date" @input="onUpdate" mode='single' />
+  </div>
+</template>
+
+<script>
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/lib/v-calendar.min.css';
+
+// Remember to setup calendar (passing in defaults if needed)
+setupCalendar({
+  firstDayOfWeek: 2
+});
+
+export default {
+  inheritAttrs: false,
+
+  props: {
+    value: Date
+  },
+
+  data() {
+    return {
+      date: null
+    };
+  },
+
+  mounted () {
+    this.date = this.value ? this.value : null
+  },
+
+  components: {
+    VDatePicker: DatePicker
+  },
+
+  methods: {
+    onUpdate (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
