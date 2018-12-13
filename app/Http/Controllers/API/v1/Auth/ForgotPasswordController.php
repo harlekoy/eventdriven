ce4c2f11e3c7f879b59757121d5df41b1c9208d1
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-12-13 17:27:55 +0800

    Fix issue during forgot password using the wrong email

--- app/Http/Controllers/API/v1/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/API/v1/Auth/ForgotPasswordController.php
@@ -45,4 +45,18 @@ class ForgotPasswordController extends Controller
             'message' => trans($response),
         ]);
     }
+
+    /**
+     * Get the response for a failed password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return response()->json([
+            'message' => trans($response),
+        ]);
+    }
 }
